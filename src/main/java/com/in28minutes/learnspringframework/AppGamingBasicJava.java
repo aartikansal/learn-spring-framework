fdd1f802fd08271ca02dc615342bc7db6abae675
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-17 13:38:27 -0500

    Gaming Application Added Mario Game Added SuperContraGame

--- src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -1,13 +1,15 @@
 package com.in28minutes.learnspringframework;
 
+import com.in28minutes.learnspringframework.game.SuperContraGame;
 import org.springframework.boot.SpringApplication;
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        var marioGame= new MarioGame();
-        var gameRunner=new GameRunner(marioGame);
+        //var marioGame= new MarioGame();
+        var superContraGame = new SuperContraGame();
+        var gameRunner=new GameRunner(superContraGame);
         gameRunner.run();
     }
 }
