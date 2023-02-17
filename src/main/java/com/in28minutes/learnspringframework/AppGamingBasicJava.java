572db8980779538c50683e153273f16e89a8b5e8
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-17 14:51:27 -0500

    Gaming Application Added Mario Game Added SuperContraGame Created Interface GamingConsole Added Pacman Game

--- src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -1,5 +1,6 @@
 package com.in28minutes.learnspringframework;
 
+import com.in28minutes.learnspringframework.game.PacmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 import org.springframework.boot.SpringApplication;
 import com.in28minutes.learnspringframework.game.GameRunner;
@@ -7,9 +8,11 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        //var marioGame= new MarioGame();
-        var superContraGame = new SuperContraGame();
-        var gameRunner=new GameRunner(superContraGame);
+        //var game= new MarioGame(); 1.Object creation
+        //var game= new SuperContraGame();
+        var game = new PacmanGame();
+        var gameRunner=new GameRunner(game);
+        //Game is a dependency of GameRunner class
         gameRunner.run();
     }
 }
