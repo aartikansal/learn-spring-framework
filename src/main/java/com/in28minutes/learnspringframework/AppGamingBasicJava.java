ae2fa3e1325353e350e8bbbd6b1adcfcf1d5907f
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-17 14:55:07 -0500

    Gaming Application Added Mario Game Added SuperContraGame Created Interface GamingConsole Added Pacman Game Added descriptive comments

--- src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -8,10 +8,12 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        //var game= new MarioGame(); 1.Object creation
+        //var game= new MarioGame(); //1.Object creation
         //var game= new SuperContraGame();
         var game = new PacmanGame();
-        var gameRunner=new GameRunner(game);
+        var gameRunner = new GameRunner(game);
+        //2: Object creation + Wiring of Dependencies
+        //passing game parameter to the constructor of Game Runner
         //Game is a dependency of GameRunner class
         gameRunner.run();
     }
