03227f42051bfbd983d129190f383d69db06d259
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-17 18:05:29 -0500

    Gaming Application Added Mario Game Added SuperContraGame Created Interface GamingConsole Added Pacman Game Added descriptive comments Added Spring beans Added configuration class

--- src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -1,17 +1,31 @@
 package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
 import com.in28minutes.learnspringframework.game.PacmanGame;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.text.Annotation;
 
 public class App03GamingSpringBeans {
     public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+    }
+}
+
         //var game= new MarioGame(); //1.Object creation
         //var game= new SuperContraGame();
-        var game = new PacmanGame();
-        var gameRunner = new GameRunner(game);
+        //var game = new PacmanGame();
+        //var gameRunner = new GameRunner(game);
         //2: Object creation + Wiring of Dependencies
         //passing game parameter to the constructor of Game Runner
         //Game is a dependency of GameRunner class
-        gameRunner.run();
-    }
-}
+       // gameRunner.run();
+
+
+
+
+
